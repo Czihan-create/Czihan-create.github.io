a7670e579787a108ca66a08edb09ce3cecb8bd71
Czihan-create <Chenzihan0227@126.com>
2025-08-23 15:47:22 +0800

    Site updated: 2025-08-23 15:47:21

---
+++ js/kbn_set.js
@@ -0,0 +1,15 @@
+// 先配置再加载
+window.widget = {
+  model: {
+    jsonPath: 'https://fastly.jsdelivr.net/npm/live2d-widget-model-hijiki@1.0.5/assets/hijiki.model3.json'
+  },
+  display: {
+    position: 'right',
+    width: 150,
+    height: 250,
+    hOffset: 30,
+    vOffset: 50
+  },
+  mobile: { show: true },
+  log: false
+};
